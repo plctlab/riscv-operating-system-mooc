96b0391f41256e74ed7b3fb515f8aec00ba22e92
Wang Chen <wangchen20@iscas.ac.cn>
2024-03-27 17:40:52 +0800

    Add RV64 support.
    Signed-off-by: Wang Chen <wangchen20@iscas.ac.cn>

--- code/os/defines.mk
+++ code/os/defines.mk
@@ -2,6 +2,13 @@
 # Support customization in the Makefile files for each project separately.
 # And also support customization in common.mk
 
+ifeq (${ARCH}, RV64)
+DEFS += -DCONFIG_RV64
+else
+# RV32 or default is 32bit
+DEFS += -DCONFIG_RV32
+endif
+
 ifeq (${SYSCALL}, y)
 DEFS += -DCONFIG_SYSCALL
 endif
